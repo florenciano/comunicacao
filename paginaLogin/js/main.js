d86ad03ac2f5ef691f392bf3c5da6c84ab7a28f9
Rodrigo Florenciano <rodrigoflorenciano@Mac-01.local>
2014-06-10 18:16:19 -0300

    pag login day2

--- paginaLogin/js/main.js
+++ paginaLogin/js/main.js
@@ -1,2 +1,14 @@
 /* scripts da página */
-$( ".perfilUsuario" ).css( "display","none" );
+// $( ".containerReqSistema, .perfilUsuario, .bg-modal" ).css( "display","none" );
+
+// escondendo o modal: requisitos de sistema
+$( "#close-modal-reqSistema" ).click( function(ev) {
+	$( ".containerReqSistema, .bg-modal" ).css( "display","none" );
+	ev.preventDefault;
+});
+
+// escondendo o modal: perfis
+$( "#close-modal-perfil" ).click( function(ev) {
+	$( ".perfilUsuario, .bg-modal" ).css( "display","none" );
+	ev.preventDefault;
+});
