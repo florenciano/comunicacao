e68418253c898ff860078dbb4b4b7e962b2aeabc
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2014-10-14 11:33:07 -0300

    adjust bestPractice jQuery

--- infograficoHowLearningWork/html/js/main.js
+++ infograficoHowLearningWork/html/js/main.js
@@ -1,37 +1,38 @@
 $(function() {
 
-    // variables
-    var btnReadMore = $( ".btnReadMore" );
-    var btnCloseMore = $( ".closeReadMore" );
-
-    // touch
-    $( "#testEl" ).bind( "touchstart touchend", function(e) {
-        $(this).toggleClass( "testTouchOverEffects" );
-        e.preventDefault();
-    });
-
-
     /*  show/hide description 'read more' with animate
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    var btnReadMore = $( ".btnReadMore" );
+    
     function onClickToggleCont(e) {
         $(this).parent().next().slideToggle( "slow" );
         e.preventDefault();
-    }
+    };
 
-    /*  close content textual of 'read more'
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-    function onClickHideCont(e) {
-        $(this).parents( ".readMore" ).fadeOut( "slow" );
-        e.preventDefault();
-    }
-
-    // events
     btnReadMore.each(function() {
         $(this).on( "click", onClickToggleCont );
     });
 
-    btnCloseMore.each(function() {
+    /*  close content textual aditional
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    var btnCloseMore = $( ".closeReadMore" );
+
+    function onClickHideCont(e) {
+        $(this).parents( ".readMore" ).fadeOut( "slow" );
+        e.preventDefault();
+    };
+
+     btnCloseMore.each(function() {
         $(this).on( "click", onClickHideCont );
     });
+
+    /*  emule effects for media touch
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    function onTouchEffects(e) {
+        $(this).toggleClass( "testTouchOverEffects" ); // insert here new class equal class:hover 
+        e.preventDefault();
+    }
+    
+    $( [btnReadMore, btnCloseMore] ).bind( "touchstart touchend", onTouchEffects );
 
 });
