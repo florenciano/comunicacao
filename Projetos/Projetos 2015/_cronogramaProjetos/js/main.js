4b18a7076893561a698ca3febf97877dc39d0ae7
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2015-04-07 17:38:55 -0300

    cronograma projetos 2015

---
+++ Projetos/Projetos 2015/_cronogramaProjetos/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+	/////////////////////////////////////////////////////////////////////
+	//	_historico
+	/////////////////////////////////////////////////////////////////////
+
+	/* TEMP
+	var a = $( ".listHistory", ".listProject" ).find( "a" );
+	a.each( function () {
+		$(this).on( "click", function (event) {
+			event.preventDefault();
+		});
+	});
+	*/
+
+});
