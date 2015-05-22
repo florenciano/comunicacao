37c88293a944cce677c13a801f18c6cba428416d
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2015-05-22 14:55:46 -0300

    _projetos update

--- Projetos/Projetos 2015/_cronogramaProjetos/assets/js/main.js
+++ Projetos/Projetos 2015/_cronogramaProjetos/assets/js/main.js
@@ -1,15 +1,5 @@
 $(function(){
 	/////////////////////////////////////////////////////////////////////
-	//	_historico
+	//	_events general
 	/////////////////////////////////////////////////////////////////////
-
-	/* TEMP
-	var a = $( ".listHistory", ".listProject" ).find( "a" );
-	a.each( function () {
-		$(this).on( "click", function (event) {
-			event.preventDefault();
-		});
-	});
-	*/
-
 });
