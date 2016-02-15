ae94825430a63926c5b00f47c81c6d71503c7fa8
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2016-02-15 16:56:16 -0200

    guia ambientacao - day 3

--- Projetos/Projetos 2015/guia-ambientacao-prof/html/T/assets/js/main.js
+++ Projetos/Projetos 2015/guia-ambientacao-prof/html/T/assets/js/main.js
@@ -10,31 +10,63 @@ $(function(){
 	"use strict";
 
 	// var global
-	var posTitle = $( "#first" ).offset().top; // <h1> = 152.9843
+	var posTitle = $( "#first" ).offset().top; // ~ 152 <h1>
 
-	// // fixed menu-lateral in scrool and (.header-fixed)
+	// // Fixar header quando rola a página. Somente em desktop
+	// // Tirar os alvos dos links âncoras debaixo do header
 	// // // // // // // // // // // // // // // // // // // // // // //
-	function fixedMenuTop () {
+	function fixedHeader_linksAnchor () {
+		
+		// // // // // // // // // // // // // // // // // // // // // // //
+		// // Fixar header quando rolar a página. Somente em desktop.
+		
 		var menuLateral = $( ".menu-lateral" ),
-			_header = document.querySelectorAll('.header'),
-			headerHeigth = _header[0].offsetHeight; // 118
+			_header = document.querySelectorAll( '.header' ),
+			headerHeigth = _header[0].offsetHeight; // ~ 118
 
-		$( document ).bind( "ready scroll", function() {
-			// exec only view in large desktop
-			if (window.outerWidth > 1024) {
-				// fixed menu-lateral
-				// $(this).scrollTop() >= (posTitle - 20 - headerHeigth) ? menuLateral.addClass( "fixed-menu" ) : menuLateral.removeClass( "fixed-menu" );
-
-				// fixed header
-				if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) { // crossbrowser
-					_header[1].className = 'header header-fixed'; // headerUp
-				} else {
-					_header[1].className = 'header hidden'; // headerUp
+			var breakPoint = 992; // Value = $screen-md [grid system bootstrap]
+			
+			function fixedHeader () {
+				console.log('a');
+				if (window.outerWidth >= breakPoint) {
+					/*
+					fixed menu-lateral. Disabled in older reviews
+					$(this).scrollTop() >= (posTitle - 20 - headerHeigth) ? menuLateral.addClass( "fixed-menu" ) : menuLateral.removeClass( "fixed-menu" );
+					*/
+					
+					if (document.documentElement.scrollTop > 0 || document.body.scrollTop > 0) { // crossbrowser
+						_header[1].className = 'header header-fixed';
+					} else {
+						_header[1].className = 'header hidden';
+					}
 				}
 			}
+		$( document ).on( "scroll", function() { fixedHeader() });
+		$( window ).bind( "ready resize", function() { fixedHeader() });
+
+		// // // // // // // // // // // // // // // // // // // // // // //
+		// // Tirar os alvos dos links âncoras debaixo do header.
+		
+		var linksMenuLateral = menuLateral.find('a[href*="#"]');
+		
+		linksMenuLateral.each( function () {
+			$(this).on( 'click', function (event) {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			     	var target = $(this.hash);
+			     	target = target.length ? target : $( '[id=' + this.hash.slice(1) +']' );
+			     	if (target.length) {
+			        	$( 'html, body' ).animate({
+			        		scrollTop: target.offset().top - headerHeigth
+			        	}, 100);
+			        return false;
+			      }
+			    }
+			});
 		});
+
 	}
-	fixedMenuTop();
+	fixedHeader_linksAnchor();
+
 
 	// // show/hide menu-lateral-small-device
 	// // // // // // // // // // // // // // // // // // // // // // //
