b713c044ebbdbb499fa15f5c6ef4becf6f549515
Rodrigo Florencano <florenciano.rodrigo@gmail.com>
2016-12-21 15:55:46 -0200

    relatorio 121 - arquitetura informação

--- Projetos/Projetos 2016/relatorio121/html/assets/js/main.js
+++ Projetos/Projetos 2016/relatorio121/html/assets/js/main.js
@@ -1,10 +1,35 @@
 $(function(){
-    
-    // HOME
-	// // // // // // // // // // // // // // // // // // // // // // //
 	
 	"use strict";
 
 	console.log(true);
+
+	// // // // // // // // // // // // // // // // // // // // // // //
+	// PAGE: Resultados - Persona - Dedicação Exclusiva
+	// // // // // // // // // // // // // // // // // // // // // // //
+
+	// Ver Entrevista do persona Rick
+    var verEntrevistaRick = $("#btn-enterview-rick"),
+    	conteudoEntrevistaRick = $("#personas-enterview-rick");
+
+    verEntrevistaRick.on("click", function(){
+        conteudoEntrevistaRick.fadeToggle("slow");
+    });
+
+    // Ver Entrevista do persona Barbara
+    var verEntrevistaBarbara = $("#btn-enterview-barbara"),
+    	conteudoEntrevistaBarbara = $("#personas-enterview-barbara");
+
+    verEntrevistaBarbara.on("click", function(){
+        conteudoEntrevistaBarbara.fadeToggle("slow");
+    });
+
+    // Ver Entrevista do persona John
+    var verEntrevistaJohn = $("#btn-enterview-john"),
+    	conteudoEntrevistaJohn = $("#personas-enterview-john");
+
+    verEntrevistaJohn.on("click", function(){
+        conteudoEntrevistaJohn.fadeToggle("slow");
+    });
 
 });
